e105b5f81c189f01a83589fe2025fa5d8e40ee5e
Santiago Reyes <santiago_reyes@ciencias.unam.mx>
2024-02-28 22:35:27 -0600

    se avanzo la implementacion de listas por compresion

--- [LC-2024-2]Practica1.1/src/ListasPorComprension.hs
+++ [LC-2024-2]Practica1.1/src/ListasPorComprension.hs
@@ -12,10 +12,17 @@ module ListasPorComprension where
 --------------------------------------------------------------------------------
 
 -- | EJERCICIO 1 | Conjunto potencia
+powerset :: [a] -> [[a]]
+powerset [] = [[]]
+powerset (x:xs) = powerset xs ++ map (x:) (powerset xs)
 
 -- | EJERCICIO 2 | Lista que regrese los elementos del 0 al n
+elementsToN :: Int -> [Int]
+elementsToN n = [0..n]
 
 -- | EJERCICIO 3 | Lista que regrese los elementos pares hasta n
+evenNumbersToN :: Int -> [Int]
+evenNumbersToN n = [x | x <- [0..n], even x]
 
 -- | EJERCICIO 4 | Lista que regrese los multiplos de n hasta k
 
